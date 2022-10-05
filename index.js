07f1391d1fa541aee9daba828656b412520d78ad
julianmolina99 <juliandavidmv@ufps.edu.co>
2022-10-05 17:08:46 -0500

    separation of responsibilities with express.Router

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('my app')
+const express = require('express');
+const routerApi = require('./routes');
+
+
+const app = express();
+const port = 3007;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!, my first server at express');
+
+});
+
+app.get('/new-route', (req, res) => {
+  res.send('Hello World!,I am a new route');
+});
+
+
+routerApi(app);
+
+
+app.listen(port, () => {
+  console.log('Server is running at port 3007');
+});
