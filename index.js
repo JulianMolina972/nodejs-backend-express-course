9251e8b0f9e3e1dd86466895234454cacb41b073
julianmolina99 <juliandavidmv@ufps.edu.co>
2022-10-05 17:35:22 -0500

    POST method and parent path

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const routerApi = require('./routes');
 const app = express();
 const port = 3007;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World!, my first server at express');
 
